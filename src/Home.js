a29ad216470dabeb7da62fe50ad1baeda4480b3e
Semyon <yanashifrakh@gmail.com>
2020-03-16 23:25:26 +0300

    another commit

--- src/Home.js
+++ src/Home.js
@@ -26,14 +26,14 @@ class Home extends React.Component {
     });
   };
 
-  addFavourites = id => {
+  addFavourites = city => {
     // this.state.favourites.push(
     //   this.state.cities[id].title,
     //   this.state.cities[id].woeid
     // );
     this.state.favourites.push({
-      title: this.state.cities[id].title,
-      woeid: this.state.cities[id].woeid
+      title: city.title,
+      woeid: city.woeid
     });
 
     this.setState({
@@ -42,17 +42,13 @@ class Home extends React.Component {
     console.log(this.state.favourites);
   };
 
-  deleteFromFavourites = id => {
-    for (let i = 0; i < this.state.favourites.length - 1; i++) {
-      // тут хуйня
-      if (this.state.favourites[i] === id) this.state.favourites.splice(id, 1);
-      console.log(this.state.cities[id]);
-    }
+  deleteFromFavourites = city => {
+    this.state.favourites.filter(city => city !== city.woeid);
     this.setState({
       favourites: this.state.favourites
     });
+    console.log(city);
     console.log(this.state.favourites);
-    console.log(this.state.favourites[id]);
   };
 
   render() {
@@ -69,10 +65,11 @@ class Home extends React.Component {
         </div>
         <div className="list">
           {this.state.cities.map((city, i) => (
-            <div key={i}>
+            <div key={city.title}>
               <Link to={`/city/${city.woeid}`}>{city.title}</Link>
-              <button onClick={() => this.addFavourites(i)}>Add</button>
-              <button onClick={() => this.deleteFromFavourites(i)}>
+
+              <button onClick={() => this.addFavourites(city)}>Add</button>
+              <button onClick={() => this.deleteFromFavourites(city)}>
                 Delete
               </button>
               <br></br>
@@ -103,3 +100,20 @@ export default Home;
 // this.state.favourites.filter(el => {
 //   return el !== el[id];
 // });
+
+// this.state.favourites.filter(el => el.i != i);
+
+// this.setState(({ favourites }) => {
+//   const idx = favourites.findIndex(el => el.id === id);
+//   const newFavoutires = [
+//     ...favourites.slice(0, idx),
+//     favourites.slice(idx + 1)
+//   ];
+//   return {
+//     favourites: newFavoutires
+//   };
+// });
+
+// this.setState(favourites => ({
+//   favourites: [favourites.slice(0, id), favourites.slice(id + 1)]
+// }));
